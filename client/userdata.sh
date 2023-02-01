485f36b25674099dd92e4d54ca8f0f9b680ebe31
José Emilio Vera <68351292+joseemilio-vera@users.noreply.github.com>
2023-02-01 14:00:30 +0100

    Update userdata.sh

--- client/userdata.sh
+++ client/userdata.sh
@@ -4,6 +4,14 @@ export http_proxy=http://<punto-enlace>:3128
 export https_proxy=http://<punto-enlace>:3128
 echo "http_proxy=http://<punto-enlace>:3128" >> /etc/environment
 echo "https_proxy=http://<punto-enlace>:3128" >> /etc/environment
+mkdir -p /etc/systemd/system/amazon-ssm-agent.service.d
+cat <<EOF >/etc/systemd/system/amazon-ssm-agent.service.d/override.conf
+[Service]
+Environment="http_proxy=http://<punto-enlace>:3128"
+Environment="https_proxy=http://<punto-enlace>:3128"
+Environment="no_proxy=169.254.169.254"
+EOF
 yum update -y
 yum install lynx -y
-reboot
+sudo systemctl daemon-reload
+systemctl restart amazon-ssm-agent
